a6442d6262caafb8b2f8bfd477ea003f9c5ecb4b
jorge <jorgea_centeno@hotmail.com>
2017-06-27 20:52:58 +0200

    first awsome commit

--- sql/TRIG_all.sql
+++ sql/TRIG_all.sql
@@ -47,3 +47,39 @@ BEGIN
         set MESSAGE_TEXT = msg, MYSQL_ERRNO=3002;
     end if;
 END$$
+
+delimiter $$
+drop trigger if exists prevent_answer_before_evaluation_time$$
+CREATE trigger `prevent_answer_before_evaluation_time` 
+before insert on sql_quiz.sql_answer
+for each row
+BEGIN
+	declare start_time datetime;
+    declare msg varchar(100);
+    
+    select scheduled_at into start_time from evaluation where evaluation_id = new.evaluation_id;
+    
+    if now() < start_time then
+		set msg = concat("It is too soon to answer questions. Start time: ",start_time);
+        signal sqlstate '45000'
+        set MESSAGE_TEXT = msg, MYSQL_ERRNO=3003;
+    end if;
+END$$
+
+delimiter $$
+drop trigger if exists prevent_answer_after_evaluation_ending_time$$
+CREATE trigger `prevent_answer_after_evaluation_ending_time` 
+before insert on sql_quiz.sql_answer
+for each row
+BEGIN
+	declare ending_time datetime;
+    declare msg varchar(100);
+    
+    select ending_at into ending_time from evaluation where evaluation_id = new.evaluation_id;
+    
+    if now() > ending_time then
+		set msg = concat("It is too late to answer questions. End time: ",ending_time);
+        signal sqlstate '45000'
+        set MESSAGE_TEXT = msg, MYSQL_ERRNO=3004;
+    end if;
+END$$
